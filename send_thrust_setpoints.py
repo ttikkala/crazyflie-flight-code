6d87a797371f634382de2eea3562e4ad60a3548f
Tiia <tiia.tikkala@gmail.com>
2023-06-14 16:49:11 +0300

    Thrust set point sending from file

--- send_thrust_setpoints.py
+++ send_thrust_setpoints.py
@@ -1,5 +1,8 @@
 import logging
 import time
+import csv
+import pandas as pd
+import os
 
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
@@ -12,33 +15,106 @@ uri = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
 # Only output errors from the logging framework
 logging.basicConfig(level=logging.ERROR)
 
-thrust_file = '~/.config/cfclient/logdata/20230614T11-16-14/Motors-20230614T11-16-23.csv'
+thrust_file = '~/.config/cfclient/logdata/20230614T16-19-03/Motors-20230614T16-19-15.csv'
+data = pd.read_csv(thrust_file)
+
+m1_input = data['motor.m1'].tolist()
+m2_input = data['motor.m2'].tolist()
+m3_input = data['motor.m3'].tolist()
+m4_input = data['motor.m4'].tolist()
+
+stab_thrust_input = data['stabilizer.thrust'].tolist()
+
+stab = 0
+motor_signals = [0, 0, 0, 0]
+
+
+def log_stab_callback(timestamp, data, logconf):
+    print(data)
+    global stab
+    stab = data['stabilizer.thrust']
+
+    # file_path = './' + 'experiment_data' + '/' + file_extension
+
+    # with open(os.path.join(file_path,
+    #         'data.csv'), 'a') as fd:
+    #     cwriter = csv.writer(fd)
+    #     cwriter.writerow([time.time(), stab, 0, 0, 0, 0]) # time.time() is time since 'epoch' - Jan 1 1970 00:00
+
+
+def log_motor_callback(timestamp, data, logconf):
+    print(data)
+    global motor_signals
+    motor_signals[0] = data['motor.m1']
+    motor_signals[1] = data['motor.m2']
+    motor_signals[2] = data['motor.m3']
+    motor_signals[3] = data['motor.m4']
+
+
+    file_path = './' + 'experiment_data' + '/' + file_extension
+
+    with open(os.path.join(file_path,
+            'data.csv'), 'a') as fd:
+        cwriter = csv.writer(fd)
+        cwriter.writerow([time.time(), stab, motor_signals[0], motor_signals[1], motor_signals[2], motor_signals[3]]) # time.time() is time since 'epoch' - Jan 1 1970 00:00
 
 
 
-def simple_log(scf, logconf):
 
-    with SyncLogger(scf, lg_stab) as logger_stab: 
-        with SyncLogger(scf, lg_motor) as logger_motor:
+def thrust_ramp(scf):
 
-            for log_entry_stab in logger_stab:
-                for log_entry_motor in logger_motor:
+    thrust_mult = 1
+    thrust_step = 500
+    thrust = 20000
+    pitch = 0
+    roll = 0
+    yawrate = 0
 
-                    timestamp_stab = log_entry_stab[0]
-                    data_stab = log_entry_stab[1]
-                    logconf_name_stab = log_entry_stab[2]
+    # Unlock startup thrust protection
+    scf.cf.commander.send_setpoint(0, 0, 0, 0)
 
-                    timestamp_motor = log_entry_motor[0]
-                    data_motor = log_entry_motor[1]
-                    logconf_name_motor = log_entry_motor[2]
+    while thrust >= 20000:
+        scf.cf.commander.send_setpoint(roll, pitch, yawrate, thrust)
+        time.sleep(0.1)
+        if thrust >= 25000:
+            thrust_mult = -1
+        thrust += thrust_step * thrust_mult
+    scf.cf.commander.send_setpoint(0, 0, 0, 0)
+    # Make sure that the last packet leaves before the link is closed
+    # since the message queue is not flushed before closing
+    time.sleep(0.1)
+    scf.cf.close_link()
 
-                    print('[%d][%s]: %s' % (timestamp_stab, logconf_name_stab, data_stab))
-                    print('[%d][%s]: %s' % (timestamp_motor, logconf_name_motor, data_motor))
 
-                    break
+def thrust_from_file(scf):
+
+    scf.cf.commander.send_setpoint(0, 0, 0, 0)
+
+    for thrust in stab_thrust_input:
+        time.sleep(0.1)
+        scf.cf.commander.send_setpoint(0, 0, 0, thrust) # roll, pitch, yawrate, thrust
+
+    scf.cf.commander.send_setpoint(0, 0, 0, 0)
+    time.sleep(0.1)
+    scf.cf.close_link()
 
 
 if __name__ == '__main__':
+    
+    file_extension = str(time.ctime().replace(' ', '_'))
+    folder = 'experiment_data'
+    file_path = './' + folder + '/' + file_extension
+
+    # Create experiment folder
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+    
+    with open(os.path.join(file_path,
+            'data.csv'), 'a') as fd:
+        cwriter = csv.writer(fd)
+        cwriter.writerow(['Time', 'stabilizer.thrust', 'motor.m1', 'motor.m2', 'motor.m3', 'motor.m4']) 
+    
+    
     # Initialize the low-level drivers
     cflib.crtp.init_drivers()
 
@@ -50,10 +126,25 @@ if __name__ == '__main__':
     lg_motor.add_variable('motor.m3', 'float')
     lg_motor.add_variable('motor.m4', 'float')
 
+    
 
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
 
+        scf.cf.log.add_config(lg_stab)
+        lg_stab.data_received_cb.add_callback(log_stab_callback)
+        scf.cf.log.add_config(lg_motor)
+        lg_motor.data_received_cb.add_callback(log_motor_callback)
+
         # simple_connect()
 
-        simple_log(scf, lg_stab)
-        simple_log(scf, lg_motor)
+        # simple_log(scf, lg_stab)
+        # simple_log(scf, lg_motor)
+
+        lg_motor.start()
+        lg_stab.start()
+
+        thrust_ramp(scf)
+        # thrust_from_file(scf)
+
+        lg_motor.stop()
+        lg_stab.stop()
